10fad2ac77db9730d9211589efeacf4c1e4e83ee
Dario Campaña <wdario.c7@gmail.com>
2024-05-30 12:16:09 -0500

    Folders commit important

---
+++ includes/config.php
@@ -0,0 +1,20 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| OWSA-INV V2
+|--------------------------------------------------------------------------
+| Author: Siamon Hasan
+| Project Name: OSWA-INV
+| Version: v2
+| Offcial page: http://oswapp.com/
+| facebook Page: https://www.facebook.com/oswapp
+|
+|
+|
+*/
+  define( 'DB_HOST', 'dbcafe.c8dbwo8eypho.us-east-1.rds.amazonaws.com' );          // Set database host
+  define( 'DB_USER', 'admin' );             // Set database user
+  define( 'DB_PASS', 'Formula2025**99' );             // Set database password
+  define( 'DB_NAME', 'INV_SYS' );        // Set database name
+
+?>

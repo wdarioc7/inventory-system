40ba540a6847b996e80a97c7e9a0be445243b07a
Dario Campaña <dario.campana@jikkosoft.com>
2025-02-17 19:27:14 -0500

    Change important

--- includes/config.php
+++ includes/config.php
@@ -14,7 +14,7 @@
 */
   define( 'DB_HOST', 'dbcafe.c8dbwo8eypho.us-east-1.rds.amazonaws.com' );          // Set database host
   define( 'DB_USER', 'admin' );             // Set database user
-  define( 'DB_PASS', 'Formula2025**99' );             // Set database password
+  define( 'DB_PASS', 'Formula1**99' );             // Set database password
   define( 'DB_NAME', 'INV_SYS' );        // Set database name
 
 ?>
